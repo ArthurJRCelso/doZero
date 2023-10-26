906ca35eed838de90abb6fe2188c1e83710d417f
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-10-26 22:32:45 +0100

    algoritmos

---
+++ algoritmos e logica de programação/03 - operações matemáticas/script.js
@@ -0,0 +1,14 @@
+let numberOne = Number(prompt("Digite o primeiro número: "))
+let numberTwo = Number(prompt("Digite o segundo número: "))
+
+let sum = numberOne + numberTwo 
+let sub = numberOne - numberTwo
+let multi = numberOne * numberTwo
+let div = numberOne / numberTwo
+let rest = numberOne % numberTwo
+
+alert("Soma: " + sum)
+alert("Subtração: " + sub)
+alert("Multiplicação: " + multi)
+alert("Divisão: " + div)
+alert("Resto da divisão: " + rest)
