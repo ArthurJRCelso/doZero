83a52f26a2bb4c74c647676a04783bd4af6e9ae1
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-10-26 18:13:43 +0100

    lógica e algoritmo

---
+++ algoritmos e logica de programação/02 - soma/script.js
@@ -0,0 +1,7 @@
+alert('Iremos somar dois números!')
+
+let numberOne = Number(prompt("Digite o primeiro número:"))
+let numerTwo = Number(prompt("Digite o segundo número:"))
+
+let soma = numberOne + numerTwo
+alert('A soma dos dois número é: ' + soma)
