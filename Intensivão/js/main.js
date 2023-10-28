76d2716cbcf0a08e918f988229f802ec9c205261
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-10-28 20:46:47 +0100

    att

---
+++ Intensivão/js/main.js
@@ -0,0 +1,11 @@
+let numberOne = Number(prompt('Digite um número'))
+let numberTwo = Number(prompt('Digite outro número:'))
+
+let soma = numberOne % numberTwo
+
+if(soma != 0) {
+    alert('A soma dos dois número é ímpar.')
+} else {
+    alert('A soma dos dois números é par.')
+}
+
