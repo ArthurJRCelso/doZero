39e05f0d2f5b7b50769dfe0403e18524ef38eccd
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-10-30 22:46:42 +0000

    oninput

---
+++ Avançando no Javascript/E6 Modules/main.js
@@ -0,0 +1,3 @@
+import { message, text } from './utils.js' // nessa linha ele importa e já executa o aqui
+
+console.log(message, text)
---
+++ Avançando no Javascript/E6 Modules/utils.js
@@ -0,0 +1,4 @@
+const message = 'texto' // variável isolada nesse arquivo, e não consigo usar ela em outro lugar
+const text = 'outra mensagem'
+
+export { message, text }
