a89e5b113b86a4da249ff783ab3031d317a77770
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-11-04 15:58:42 +0000

    dataset

--- Avançando no Javascript/FocusTimer2.0/js/main.js
+++ Avançando no Javascript/FocusTimer2.0/js/main.js
@@ -4,60 +4,33 @@ const buttonPlayPause = document.querySelector('.playPause')
 const buttonStop = document.querySelector('.stop')
 const buttonAddTime = document.querySelector('.addTime')
 const buttonRemoveTime = document.querySelector('.removeTime')
-let minutes = document.querySelector('.minute')
-let seconds = document.querySelector('.second')
+const controls = document.querySelector('.buttons')
 let clearTimeOutID = null
-const state = {
-    min: 25,
-    sec: 0,
+let state = {
+    minutes: 25,
+    seconds: 0,
+    isRunning: false,
+    noMusic: true, 
 }
-
-buttonStop.addEventListener('click', reset)
 
 buttonToggle.addEventListener('click', () => {
     document.documentElement.classList.toggle('dark')
 })
 
-buttonMusicOnOff.addEventListener('click', () => {
-    document.documentElement.classList.toggle('music-on')
-})
-
-buttonPlayPause.addEventListener('click', countdown)
-
-function countdown() {
-    clearTimeout(clearTimeOutID)
-
-    let minute = Number(minutes.textContent)
-    let second = Number(seconds.textContent)
-
-    second--
-
-    if(second < 0) {
-        second = 59
-        minute--
-    }
-
-    if(minute < 0) {
-        reset()
-        return
-    }
-
-    updateDisplay(minute, second)
-
-    clearTimeOutID = setTimeout(() => countdown(), 1000)
+function registerControls() {
+    controls.addEventListener('click', (event) => {
+       const action = event.target.dataset.action
+    })
 }
 
-function updateDisplay(minute, second) {
-    minute = minute ?? state.min
-    second = second ?? state.sec
+function start(minutes, seconds) {
+    state.minutes = minutes
+    state.seconds = seconds
 
-    minutes.textContent = String(minute).padStart(2, "0")
-    seconds.textContent = String(second).padStart(2, "0")
+    registerControls()
 }
 
-function reset() {
-    updateDisplay()
-}
 
+start(0, 6)
 
 
