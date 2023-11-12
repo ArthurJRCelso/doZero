28d53fb2bd6846d7f24022ae2c564abcfece43d6
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-11-12 18:20:33 +0000

    SPA

--- JavaScript antes do framework/Projeto07/js/index.js
+++ JavaScript antes do framework/Projeto07/js/index.js
@@ -1,31 +1,31 @@
-
 const routes = {
-    "/": "/pages/home.html",
-    "/about": "/pages/about.html",
-    "/contact": "/pages/contact.html",
-    404: "/pages/404.html",
-  }
+  "/": "/pages/home.html",
+  "/about": "/pages/about.html",
+  "/contact": "/pages/contact.html",
+  404: "/pages/404.html",
+}
 
-  function route(event) {
-    event = event || window.event
-    event.preventDefault()
+function route(event) {
+  event = event || window.event
+  event.preventDefault()
 
-    window.history.pushState({}, "", event.target.href)
-
-    handle()
-  }
-
-  function handle() {
-    const { pathname }  = window.location
-    const route = routes[pathname] || routes[404]
-    fetch(route)
-    .then(data => data.text())
-    .then(html => {
-      document.querySelector('#app').innerHTML = html
-    })
-  }
+  window.history.pushState({}, "", event.target.href)
 
   handle()
+}
 
-  window.onpopstate = () => handle()
-  window.route = () => route()
+function handle() {
+  const { pathname }  = window.location
+  const route = routes[pathname] || routes[404]
+  console.log('antes do fetch')
+  fetch(route)
+  .then(data => data.text())
+  .then(html => {
+    document.querySelector('#app').innerHTML = html
+  })
+}
+
+handle()
+
+window.onpopstate = () => handle()
+window.route = () => route()
