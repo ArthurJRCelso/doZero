c1b5ff8b75c8c0cc8a5a10deedef336ffcf9e073
arthur <arthur.rodrigues1784@gmail.com>
2023-12-02 23:43:08 +0000

    request.query / request.params

--- 07 - Backend/api/src/server.js
+++ 07 - Backend/api/src/server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 
 const app = express();
-app.use(express.json());
 
-app.post("/users", (request, response) => {
-    const { name, email, password } = request.body;
+app.get("/message/:id/:user", (request, response) => {
+    const { id, user } = request.params
 
-    response.json({ name, email, password });
+    response.send(`
+      Mensagem ID: ${id}.
+      Para o usuário: ${user}.
+    `);
+});
+
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query
+
+    response.send(`
+    Página: ${page}. Mostar: ${limit}`)
 })
 
 const PORT = 3333;
-app.listen(PORT, () => console.log(`Express is running PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
