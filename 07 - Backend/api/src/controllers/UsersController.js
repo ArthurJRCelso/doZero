a1482dd94c3da29271cedd54476a29c240dd4760
arthur <arthur.rodrigues1784@gmail.com>
2023-12-14 00:43:56 +0000

    ~database

--- 07 - Backend/api/src/controllers/UsersController.js
+++ 07 - Backend/api/src/controllers/UsersController.js
@@ -1,15 +1,57 @@
+const { hash } = require('bcryptjs')
 const AppError = require("../utils/AppError")
 
-class UsersController {
+const sqliteConnection = require("../database/sqlite")
 
-    create(request, response) {
+class UsersController {
+    async create(request, response) {
         const { name, email, password } = request.body;
 
-        if (!name) {
-            throw new AppError("Nome é obrigatório!")
+        const database = await sqliteConnection()
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(checkUserExists) {
+            throw new AppError('Este email já está em uso.')
         }
 
-        response.status(201).json({ name, email, password });
+        const hashedPassword = await hash(password, 8)
+
+        await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [ name, email, hashedPassword ])
+
+        return response.status(201).json()
+
+    }
+
+    async update(request, response) {
+        const { name, email } = request.body
+        const { id } = request.params
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+        if(!user) {
+            throw new AppError("Usuário não encontrado!")
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("Este email já está em uso.")
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(`
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        update_at = ?
+        WHERE id = ?`,
+        [user.name, user.email, new Date(), id]
+        );
+
+        return response.json()
     }
 }
 
